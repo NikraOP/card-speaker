34b7dc5969ecf31ff48bd9d49fa7ed7f050cbb93
NikraOP <chpok123kolispok@gmail.com>
2026-01-30 16:00:53 +0300

    Delete test-quest directory

--- test-quest/script.js
+++
@@ -1,5 +0,0 @@
-const buttonFavourites = document.querySelector('.speaker__card-favourites')
-
-buttonFavourites.addEventListener('click', () => {
-    buttonFavourites.classList.toggle('speaker__card-favourites--active')
-})
